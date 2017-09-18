2ecde36cc635a080c5a8dd20bfc81c606c762d7d
rafasalgado <rasalgadol@unal.edu.co>
2017-09-17 23:02:14 -0500

    rederigir a pagina principal luego de llenar y enviar contacto

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :contacts
+  resources :contacts, except: [:destroy, :show, :update ]
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
