2a64179d4c6aadbcf7f5b804c8a81f8cf83b77fa
rafasalgado <rasalgadol@unal.edu.co>
2017-09-17 23:11:44 -0500

    agregada gema faker y modificacion archivo db/seeds.rb para poblar la mayoria de tablas

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.3'
 
+gem 'faker', require: false
+
 # devise for login
 gem 'devise'
 # materialize
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,71 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+puts 'started loading person data'
+
+100.times do |row|
+  person.create(document: Faker::Number.number(10),
+  name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  age: Faker::Number.number(2),
+  birthDate: Faker::Date.backward(23_725),
+  civilStatus: Faker::Lorem.words.join(' '),
+  gender: Faker::Demographic.sex,
+  originLanguage: Faker::Lorem.words.join(' '),
+  domainLanguage: Faker::Lorem.words.join(' '))
+end
+puts 'finished loading person data'
+
+100.times do |row|
+  work_group.create(participant: Faker::Name.first_name,
+  proyect: Faker::Lorem.words.join(' '))
+end
+
+puts 'finished loading work_group data'
+
+100.times do |row|
+  user.create(email: Faker::Internet.email,
+  encrypted_password: Faker::Internet.password(8))
+end
+puts 'finished loading user data'
+
+100.times do |row|
+  residence.create(address: Faker::Address.street_address,
+  phone: Faker::PhoneNumber.phone_number,
+  neighborhood: Faker::Address.community,
+  locality: Faker::Address.city)
+end
+puts 'finished loading residence data'
+
+
+100.times do |row|
+  opinion.create(interest: Faker::Lorem.sentence,
+  socialOrganization: Faker::Lorem.characters(10),
+  groupsParticipation: Faker::Lorem.characters(8),
+  workGroupDecision: Faker::Lorem.sentence,
+  ideas: Faker::Lorem.paragraph,
+  activitiesParticipatedBogota: Faker::Lorem.words.join(' '),
+  whatIsParticipating: Faker::Lorem.paragraph,
+  districtParticipation: Faker::Lorem.paragraph,
+  reasonParticipation: Faker::Lorem.paragraph,
+  reasonNoParticipation: Faker::Lorem.paragraph,
+  adviceOnParticipation: Faker::Lorem.paragraph)
+end
+puts 'finished loading opinion data'
+
+
+100.times do |row|
+  health_service.create(name: Faker::Name.name_with_middle,
+  admissionDate: Faker::Date.between(20.weeks.ago, Date.today),
+  departureDate: Faker::Date.forward(2300),
+  regime: Faker::Lorem.characters(10))
+end
+puts 'finished loading health_service data'
+
+100.times do |row|
+  governor.create(name: Faker::Name.name,
+  startgovermentperiod: Faker::Date.between(200.weeks.ago, 15.weeks.ago),
+  endgovermentperiod: Faker::Date.forward(23))
+end
