7925cdd9acfabe215b25667f5a91b4d66a89c823
Jhon Torres <jhetorresto@unal.edu.co>
2017-10-10 21:48:31 -0500

    se arregla problema de turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
 //= require jquery
+//= require jquery_ujs
+//= require jquery.turbolinks
 //= require materialize-sprockets
 //= require underscore
 //= require gmaps/google
