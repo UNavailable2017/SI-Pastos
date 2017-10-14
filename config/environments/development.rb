1d9caf73ef810f246640eccf8bf6e2d240109744
Jhon Torres <jhetorresto@unal.edu.co>
2017-10-13 23:38:51 -0500

    arreglando el mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               ENV['GMAIL_DOMAIN'],
     user_name:            ENV['email_platform'],
     password:             ENV['password_email'],
     authentication:       'plain',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               ENV['url_app'],
+    domain:               ENV['GMAIL_DOMAIN'],
     user_name:            ENV['email_platform'],
     password:             ENV['password_email'],
     authentication:       'plain',
