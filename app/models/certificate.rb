218f26f04b03716cd88da1221778cdd3c4808309
rafasalgado <rasalgadol@unal.edu.co>
2017-10-23 18:36:58 -0500

    arreglando despliege para certificado

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -12,14 +12,14 @@
 class Certificate < ApplicationRecord
   def self.data_person
     certi_last = Certificate.last
-    User.select("documentPerson", "firstname", "lastname", "documentPerson", "documentType").joins(:person).where("isCensus=? and documentPerson =?", "t", certi_last.documentPerson)
+    User.select("documentPerson", "firstname", "lastname", "documentPerson", "documentType").joins(:person).where("\"isCensus\"=? and \"documentPerson\" =?", "t", certi_last.documentPerson)
 
   end
 
   def self.check_censo
       get_document_cert = Certificate.where(["email = ?", ("#{User.current.email}")]).last
       document= get_document_cert.documentPerson
-      User.select("documentPerson", "firstname", "lastname", "documentPerson", "documentType").joins(:person).where("email= ? and isCensus=? and documentPerson =?",("#{User.current.email}"), "t", document)
+      User.select("documentPerson", "firstname", "lastname", "documentPerson", "documentType").joins(:person).where("email= ? and \"isCensus\"=? and \"documentPerson\" =?",("#{User.current.email}"), "t", document)
   end
 
 
