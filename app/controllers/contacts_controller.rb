12c9b357a87759e5b4e6c0abea5232bf5fbe1a7c
Jhon Torres <jhetorresto@unal.edu.co>
2017-10-28 10:50:27 -0500

    ordenando las rutas

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -22,16 +22,13 @@ class ContactsController < ApplicationController
   # POST /contacts.json
   def create
     @contact = Contact.new(contact_params)
-
     respond_to do |format|
       if @contact.save
         UserMailer.contact_email(@contact).deliver_now
         @contact.destroy
         format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
-        format.json { render :show, status: :created, location: @contact }
       else
         format.html { render :new }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
       end
     end
   end
