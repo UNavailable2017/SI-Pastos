5aa6ca96726601d3fd1b4c2d443e2eb84462356f
Jhon Torres <jhetorresto@unal.edu.co>
2017-11-09 15:53:33 -0500

    arreglando migraciones

--- db/migrate/20170921165236_create_announcements.rb
+++ db/migrate/20170921165236_create_announcements.rb
@@ -1,7 +1,6 @@
 class CreateAnnouncements < ActiveRecord::Migration[5.1]
   def change
     create_table :announcements do |t|
-      t.integer :idAnnouncement
       t.date :publicationDate
       t.date :deadline
       t.text :announcementType
--- db/migrate/20170922000104_remove_id_announcement_from_announcement.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIdAnnouncementFromAnnouncement < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :announcements, :idAnnouncement, :integer
-  end
-end
