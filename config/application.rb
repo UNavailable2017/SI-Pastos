07d3c506bc2d9ad3db7654eccf54bef96f113a8d
Jhon Torres <jhetorresto@unal.edu.co>
2017-12-17 23:48:04 -0500

    se configuran el active job con rescue

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,7 @@ module SIPastos
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :es
+
+    config.active_job.queue_adapter = :resque
   end
 end
